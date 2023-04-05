dd043cdf82c500266f5d04a72a285775af5c2388
Bobby Luly <bobbyluly@gmail.com>
2023-04-05 16:26:15 -0600

    Add all methods in merchant_service to get a list of all merchants

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -1,3 +1,17 @@
 class MerchantService
-  
+
+  def conn
+    Faraday.new(url: "http://localhost:3000/api/v1/") # do |faraday|
+      #faraday.adapter Faraday.default_adapter
+    # end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def all_merchants
+    get_url("merchants")
+  end
 end
