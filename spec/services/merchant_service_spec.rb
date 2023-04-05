df03b91a19a4361b7b476cea34b9e8abeae526e1
Bobby Luly <bobbyluly@gmail.com>
2023-04-05 16:25:35 -0600

    Refactor expects to get into :data level of response

--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -2,28 +2,28 @@ require "rails_helper"
 
 describe MerchantService do
   describe "#all_merchants" do
-    before do
-    end
+    # before do
+    # end
 
     it "returns all merchants" do
       merchants = MerchantService.new.all_merchants
 
-      expect(merchants).to be_an(Array)
+      expect(merchants[:data]).to be_an(Array)
 
-      expect(merchants.first).to have_key(:id)
-      expect(merchants.first[:id]).to be_a(String)
+      expect(merchants[:data].first).to have_key(:id)
+      expect(merchants[:data].first[:id]).to be_a(String)
 
-      expect(merchants.first).to have_key(:type)
-      expect(merchants.first[:type]).to be_a(String)
+      expect(merchants[:data].first).to have_key(:type)
+      expect(merchants[:data].first[:type]).to be_a(String)
 
-      expect(merchants.first).to have_key(:attributes)
-      expect(merchants.first[:attributes]).to be_a(Hash)
+      expect(merchants[:data].first).to have_key(:attributes)
+      expect(merchants[:data].first[:attributes]).to be_a(Hash)
 
-      expect(merchants.first[:attributes]).to have_key(:name)
-      expect(merchants.first[:attributes][:name]).to be_a(String)
-      expect(merchants.first[:attributes][:name]).to eq("Schroeder-Jerde")
-      expect(merchants.second[:attributes][:name]).to eq("Klein, Rempel and Jones")
-      expect(merchants.third[:attributes][:name]).to eq("Willms and Sons")
+      expect(merchants[:data].first[:attributes]).to have_key(:name)
+      expect(merchants[:data].first[:attributes][:name]).to be_a(String)
+      expect(merchants[:data].first[:attributes][:name]).to eq("Schroeder-Jerde")
+      expect(merchants[:data].second[:attributes][:name]).to eq("Klein, Rempel and Jones")
+      expect(merchants[:data].third[:attributes][:name]).to eq("Willms and Sons")
     end
   end
 end
