b710db5a121beefe356c9e856c1d2e1b7d04d209
Bobby Luly <bobbyluly@gmail.com>
2023-04-06 10:35:52 -0600

    Test: create test for #merchant_items

--- spec/services/rails_engine_service_spec.rb
+++ spec/services/rails_engine_service_spec.rb
@@ -9,8 +9,23 @@ describe RailsEngineService do
       expect(merchants_response.first.keys).to eq([:id, :type, :attributes])
       expect(merchants_response.first[:attributes]).to be_a(Hash)
       expect(merchants_response.first[:attributes]).to have_key(:name)
-      expect(merchants_response.first[:attributes][:name]).to be_a(String)
       expect(merchants_response.first[:attributes][:name]).to eq("Schroeder-Jerde")
     end
   end
+
+  describe "#merchant_items" do
+    it "returns all items for a merchant" do
+      merchant_items_response = RailsEngineService.new.merchant_items(1)
+
+      expect(merchant_items_response).to be_an(Array)
+      expect(merchant_items_response.first.keys).to eq([:id, :type, :attributes])
+      expect(merchant_items_response.first[:attributes]).to be_a(Hash)
+      expect(merchant_items_response.first[:attributes].keys).to eq([:name, :description, :unit_price, :merchant_id])
+      expect(merchant_items_response.first[:attributes][:name]).to eq("Item Nemo Facere")
+      expect(merchant_items_response.first[:attributes][:description]).to eq("Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.")
+      expect(merchant_items_response.first[:attributes][:unit_price]).to eq(42.91)
+      expect(merchant_items_response.first[:attributes][:merchant_id]).to eq(1)
+    end
+  end
+
 end
