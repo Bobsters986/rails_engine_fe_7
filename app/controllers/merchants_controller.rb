f830826b0fc7fd98915e56ba8a706398f0d7d423
Bobby Luly <bobbyluly@gmail.com>
2023-04-06 14:20:13 -0600

    Feat: adds all functionality to display a merchant's show page and their items

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,23 @@
 class MerchantsController < ApplicationController
   def index
-    @facade = RailsEngineFacade.new
+    @merchants = all_merchants
+  end
+
+  def show
+    @merchant = all_merchants.find do |merchant| 
+      merchant.id == params[:id]
+    end
+
+    @items = facade.merchant_items(params[:id])
+  end
+
+  private
+
+  def all_merchants
+    @merchants ||= facade.all_merchants
+  end
+
+  def facade
+    @facade ||= RailsEngineFacade.new
   end
 end
