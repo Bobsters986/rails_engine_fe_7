174547367aea7530e16f84f6605bad60ecc043c8
Bobby Luly <bobbyluly@gmail.com>
2023-04-06 10:48:01 -0600

    Test: create test for #merchant_items

--- spec/facades/rails_engine_facade_spec.rb
+++ spec/facades/rails_engine_facade_spec.rb
@@ -11,4 +11,14 @@ describe RailsEngineFacade do
     end
   end
 
+  describe "#merchant_items" do
+    it "returns an array of item objects" do
+      expect(facade.merchant_items(1)).to be_an(Array)
+      expect(facade.merchant_items(1).first).to be_a(Item)
+      expect(facade.merchant_items(1).first.name).to eq("Item Nemo Facere")
+      expect(facade.merchant_items(1).first.description).to eq("Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.")
+      expect(facade.merchant_items(1).first.unit_price).to eq(42.91)
+      expect(facade.merchant_items(1).first.merchant_id).to eq(1)
+    end
+  end
 end
