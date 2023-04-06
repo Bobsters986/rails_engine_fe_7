173036625b9c6209ac91a92fb006d765ff651de5
Bobby Luly <bobbyluly@gmail.com>
2023-04-06 10:59:08 -0600

    Feat: create merchant_items methods for service and facade

--- app/facades/rails_engine_facade.rb
+++ app/facades/rails_engine_facade.rb
@@ -1,11 +1,23 @@
 class RailsEngineFacade
+  attr_reader :service
+
+  def initialize
+    @service = RailsEngineService.new
+  end
 
   def all_merchants
-    service = RailsEngineService.new
-    merchants = service.all_merchants
+    merchants_info = service.all_merchants
 
-    merchants[:data].map do |merchant|
-      Merchant.new(merchant)
+    merchants_info.map do |info|
+      Merchant.new(info)
+    end
+  end
+
+  def merchant_items(merchant_id)
+    items_info = service.merchant_items(merchant_id)
+
+    items_info.map do |info|
+      Item.new(info)
     end
   end
 end
--- app/services/rails_engine_service.rb
+++ app/services/rails_engine_service.rb
@@ -1,15 +1,20 @@
 class RailsEngineService
 
+  def all_merchants
+    response = conn.get("merchants")
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed[:data]
+  end
+
+  def merchant_items(merchant_id)
+    response = conn.get("merchants/#{merchant_id}/items")
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed[:data]
+  end
+
+  private
+
   def conn
     Faraday.new(url: "http://localhost:3000/api/v1/")
-  end
-
-  def get_url(url)
-    response = conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def all_merchants
-    get_url("merchants")
   end
 end
