6eea36523c70424ff96e0ac2f918d143f227827d
Bobby Luly <bobbyluly@gmail.com>
2023-04-06 10:24:42 -0600

    Refactor expects

--- spec/services/rails_engine_service_spec.rb
+++ spec/services/rails_engine_service_spec.rb
@@ -3,24 +3,14 @@ require "rails_helper"
 describe RailsEngineService do
   describe "#all_merchants" do
     it "returns all merchants" do
-      merchants = RailsEngineService.new.all_merchants
+      merchants_response = RailsEngineService.new.all_merchants
 
-      expect(merchants[:data]).to be_an(Array)
-
-      expect(merchants[:data].first).to have_key(:id)
-      expect(merchants[:data].first[:id]).to be_a(String)
-
-      expect(merchants[:data].first).to have_key(:type)
-      expect(merchants[:data].first[:type]).to be_a(String)
-
-      expect(merchants[:data].first).to have_key(:attributes)
-      expect(merchants[:data].first[:attributes]).to be_a(Hash)
-
-      expect(merchants[:data].first[:attributes]).to have_key(:name)
-      expect(merchants[:data].first[:attributes][:name]).to be_a(String)
-      expect(merchants[:data].first[:attributes][:name]).to eq("Schroeder-Jerde")
-      expect(merchants[:data].second[:attributes][:name]).to eq("Klein, Rempel and Jones")
-      expect(merchants[:data].third[:attributes][:name]).to eq("Willms and Sons")
+      expect(merchants_response).to be_an(Array)
+      expect(merchants_response.first.keys).to eq([:id, :type, :attributes])
+      expect(merchants_response.first[:attributes]).to be_a(Hash)
+      expect(merchants_response.first[:attributes]).to have_key(:name)
+      expect(merchants_response.first[:attributes][:name]).to be_a(String)
+      expect(merchants_response.first[:attributes][:name]).to eq("Schroeder-Jerde")
     end
   end
 end
