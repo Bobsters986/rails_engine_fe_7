ae4c77b7b8198472aaf076e8d694b7352d12d714
Bobby Luly <bobbyluly@gmail.com>
2023-04-06 10:57:21 -0600

    Feat+Test: create item_poro test and item poro

---
+++ app/poros/item.rb
@@ -0,0 +1,15 @@
+class Item
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :merchant_id
+
+  def initialize(item_info)
+    @id = item_info[:id].to_i
+    @name = item_info[:attributes][:name]
+    @description = item_info[:attributes][:description]
+    @unit_price = item_info[:attributes][:unit_price]
+    @merchant_id = item_info[:attributes][:merchant_id]
+  end
+end
